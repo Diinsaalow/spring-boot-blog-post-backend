007270295ae9b8ca98b2461a39af91d46411e505
Ismail Abdifadil <ismail.haabare@gmail.com>
2025-07-07 06:32:16 +0300

    initial commit

---
+++ src/main/java/com/capstone/springbootblogpostbackend/SpringBootBlogPostBackendApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.springbootblogpostbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogPostBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBlogPostBackendApplication.class, args);
+    }
+
+}
